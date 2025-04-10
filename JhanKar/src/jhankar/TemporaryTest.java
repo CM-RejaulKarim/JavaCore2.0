5b183187473167138b5cded26724aea8008d22eb
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-04-10 19:05:26 +0600

    Method

--- JhanKar/src/jhankar/TemporaryTest.java
+++ JhanKar/src/jhankar/TemporaryTest.java
@@ -1,4 +1,3 @@
-
 package jhankar;
 
 import java.util.Scanner;
@@ -7,7 +6,24 @@ public class TemporaryTest {
 
     public static void main(String[] args) {
 
-       
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+    }
+
+    public static long doContinue(int input) {
+        int count = 1;
+        
+        
+        while (count <= input) {
+            if (count>=input) {
+                count++;
+                break;
+            } else {
+                System.out.println(count);
+                count++;
+            }
+        }
     }
 
 }
