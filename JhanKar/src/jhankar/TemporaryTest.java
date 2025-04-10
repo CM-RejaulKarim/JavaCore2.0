4ff49746c110addf1146795ec9f12cc66b0b661e
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-04-10 23:57:10 +0600

    Home

--- JhanKar/src/jhankar/TemporaryTest.java
+++ JhanKar/src/jhankar/TemporaryTest.java
@@ -7,22 +7,21 @@ public class TemporaryTest {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        
+
         System.out.println("Enter a number");
         int n = s.nextInt();
-        
+
         long counter = doContinue(n);
-        
-        System.out.println("Number is "+counter);
+
+        System.out.println(counter);
 
     }
 
     public static long doContinue(int input) {
         int count = 1;
-        
-        
+
         while (count < input) {
-            if (count>=input) {
+            if (count >= input) {
                 count++;
                 break;
             } else {
