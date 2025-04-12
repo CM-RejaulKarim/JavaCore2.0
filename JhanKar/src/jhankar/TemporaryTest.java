c1bf6e3c4fd83b78271e55a03566e97c1b60c269
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-04-12 18:58:48 +0600

    old work

--- JhanKar/src/jhankar/TemporaryTest.java
+++ JhanKar/src/jhankar/TemporaryTest.java
@@ -8,13 +8,94 @@ public class TemporaryTest {
 
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter a number");
-        int n = s.nextInt();
+        System.out.println("Enter a date");
+        int day = s.nextInt();
 
-        long counter = doContinue(n);
+        System.out.println("Enter Month");
+        int mon = s.nextInt();
 
-        System.out.println(counter);
+        System.out.println("Enter Year");
+        int year = s.nextInt();
 
+        String result = dayMonYear(day, mon, year);
+
+        System.out.println(result);
+
+    }
+
+    public static String dayMonYear(int day, int mon, int year) {
+
+        String dayAft = "";
+        String monAft = "";
+        String yearAft = "";
+        String result = dayAft + monAft + yearAft;
+
+        if (day > 31) {
+            dayAft = "Invalid Date.";
+        } else {
+            dayAft = day + "";
+        }
+
+        switch (mon) {
+            case 1:
+                monAft = "January";
+
+                break;
+            case 2:
+                monAft = "February";
+
+                break;
+            case 3:
+                monAft = "March";
+
+                break;
+            case 4:
+                monAft = "April";
+
+                break;
+            case 5:
+                monAft = "May";
+
+                break;
+            case 6:
+                monAft = "June";
+
+                break;
+            case 7:
+                monAft = "July";
+
+                break;
+            case 8:
+                monAft = "August";
+
+                break;
+            case 9:
+                monAft = "September";
+
+                break;
+            case 10:
+                monAft = "October";
+
+                break;
+            case 11:
+                monAft = "November";
+
+                break;
+            case 12:
+                monAft = "December";
+
+                break;
+            default:
+                monAft = "Invalid";
+        }
+
+//        if (year < 2025) {
+//            yearAft = "you are going to future";
+//        } else {
+//            yearAft = year + "";
+//        }
+
+        return result;
     }
 
     public static long doContinue(int input) {
