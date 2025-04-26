514c35e3c2237148ee7e0a0e016f01f776ef2bcd
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-04-26 19:02:47 +0600

    dont know

---
+++ LoopOne/src/loopone/LoopOne.java
@@ -0,0 +1,22 @@
+package loopone;
+
+public class LoopOne {
+
+    public static void main(String[] args) {
+
+//        int n = 10;
+        int m = 2;
+//        int result = 0;
+//        for (int i = 1; i <= n; i++) {
+//            result = (i * m);
+//            if (result >= 2 && result <= 20) {
+//                System.out.println(result);
+//            }
+//        }
+        
+        do {            
+            
+        } while (true);
+    }
+
+}
